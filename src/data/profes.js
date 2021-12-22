0c34772a3c423c0b99f6666bfddc0688e1984e92
ferraclosa <ferraclosa@gmail.com>
2021-12-22 15:48:41 +0100

    some more language

--- src/data/profes.js
+++ src/data/profes.js
@@ -29,6 +29,38 @@ const profes = [
             { title: "Curioso",subtitle: "",  description: "La curiosidad es uno de mis vicios. Todo me interesa. Me gusta aprender, buscar y difruto de los saltos que provoca el descubrir algo que no sabias. Las fronteras de mi ignorancia son mi terreno por descubrir, aun no explorado." }, 
         ]
 }, 
+{
+    language: "ca",
+        title: "Ferran Closa Cervera",
+        firstWord: "la meva  ",
+        spannedWord: "Experiencia",
+        intro: "Enthusiast and with a thirty-year experience, on areas like commerce, showbusiness, logistics, finance and banking. Although not initially inclined towards an IT career, I came to love its fulfilling potential for changing people's lives. conmigo.",
+        details: [
+            { title: "Espanol", subtitle: "", description: "Esto fue un accidente, pero uno afortunado.Tengo 59 anos y contando. He vivido una gran parte de mi vida en otros lugares, pero my casa sigue siendo mi tierra, mi gente y mis lugares." },
+            { title: "Musico",subtitle: "",  description: "Mi historial educacional siguio de muy pronto una vocacion por la musica, muy especialemente por el estudio del violin." },
+            { title: "Informatico",subtitle: "",  description: "Otro afortunado accidente. Lo que me atrajo, en principio fue la curiosidad y pronto, las capacidades creativas que le son associadas, como el hecho de crear algo a partir de una idea, sin mas misterios que la adopcion y combinacion de diferentes algoritmos. Esta es la faceta de my trabajo con la que mas disfruto y que me aporta mas satisfacion." }, 
+            { title: "Padre", subtitle: "", description: "No exactamente un accidente, pero sea lo que fuere, el mas feliz de los episodios de my existencia. Devenir padre debe de ser lo que menos esfuerzo require, pero ser uno de bueno, eso ya es otro cantar. La informatica, a su lado, es pan comido. Tengo tres hijas y un hijastro, por aquello de los tumbos que damos en la vida" }, 
+            { title: "Amigos", subtitle: "", description: "Soy hombre de pocos amigos, en el sentido literal de la expresion, pero los que tengo, no hay de mejores. A los amigos uno los escoge, es una relacion de libertad que puede estar ausente en las relaciones impuestas, y esta libertad es maravillosa" }, 
+            { title: "Curioso",subtitle: "",  description: "La curiosidad es uno de mis vicios. Todo me interesa. Me gusta aprender, buscar y difruto de los saltos que provoca el descubrir algo que no sabias. Las fronteras de mi ignorancia son mi terreno por descubrir, aun no explorado." }, 
+        ]
+},
+{
+    language: "fr",
+        title: "Ferran Closa Cervera",
+        firstWord: "mon ",
+        spannedWord: "Experience ",
+        intro: "Passionné et avec une expérience de trente ans, dans des domaines comme le commerce, le showbusiness, la logistique, la finance et la banque. Bien que n’étant pas initialement enclin à une carrière en informatique, j’en suis venu à aimer son potentiel épanouissant pour changer la vie des gens.",
+        details: [
+            { title: "Espagnol", subtitle: "", description: "C’était un accident, mais un accident chanceux. J’ai 59 ans et je compte. J’ai vécu une grande partie de ma vie dans d’autres endroits, mais ma maison est toujours ma terre, mon peuple et mes lieux." },
+            { title: "Musicien",subtitle: "",  description: "Mon histoire éducative a suivi très soudainement une vocation pour la musique, en particulier pour l’étude du violon." },
+            { title: "Informaticien",subtitle: "",  description: "Encore un accident chanceux. Ce qui m’a attiré, en principe, c’est la curiosité et bientôt, les capacités créatives qui lui sont associées, comme le fait de créer quelque chose à partir d’une idée, sans plus de mystères que l’adoption et la combinaison de différents algorithmes. C’est la facette de mon travail avec laquelle j’apprécie le plus et qui me donne le plus de satisfaction." }, 
+            { title: "Papà", subtitle: "", description: "Pas exactement un accident, mais quoi qu’il en soit, le plus heureux des épisodes de mon existence. Devenir père doit être ce qui demande le moins d’effort, mais être bon, c’est une autre chanson. L’informatique, à ses côtés, est un jeu d’enfant. J’ai trois filles et un beau-fils, à cause des trébuchements que nous donnons dans la vie" }, 
+            { title: "Amis", subtitle: "", description: "Je suis un homme de peu d’amis, au sens littéral de l’expression, mais ceux que j’ai, il n’y en a pas de meilleur. On choisit des amis, c’est une relation de liberté qui peut être absente dans les relations imposées, et cette liberté est merveilleuse." }, 
+            { title: "Curieux",subtitle: "",  description: "La curiosité est l’un de mes vices. Tout m’intéresse. J’aime apprendre, chercher et profiter des sauts causés par la découverte de quelque chose que vous ne saviez pas. Les frontières de mon ignorance sont mon terrain inexploré."
+
+            }, 
+        ]
+}
 ]
 
 
--- src/data/skillsdata.js
+++ src/data/skillsdata.js
@@ -5,9 +5,9 @@ const skillsdata =
             language: "en",
             title: "Ferran Closa Cervera",
             firstWord: "the",
-            spannedWord: "Skills",
-            lastWord: "",
-            intro: "The Skills I have adquired over the years, are build over my curiosity and need. For work or for personal curiosity, I have a strong analystical mind and I recognise patterns. I like travelling back and forth from the abstract to the detail. These common patterns have facilitated the learning curve for profesional, lenaguage, culture or travelling that have been a constant in my life.",
+            spannedWord: "Skills ",
+            lastWord: "I’ve learn",
+            intro: "The Skills I have acquired over the years, are built over my curiosity and need. For work or for personal curiosity, I have a strong analytical mind and I recognise patterns. I like travelling back and forth from the abstract to the detail. These common patterns have facilitated the learning curve for professional, language, culture or travelling that have been a constant in my life.",
             details: [
                 {
                     title: "IT Project Management",
@@ -18,20 +18,20 @@ const skillsdata =
                 },
                 { 
                     title: "Microservices & API", 
-                    level: "Excelent", 
+                    level: "Excellent", 
                     description: "Microservices were the reason I started moving out of legacy systems towards distributed systems. I never went back." , 
                     interest: "9/10",
                     order: 1},
                 { 
                     title: "RPG & CL", 
-                    level: "Excelent", 
-                    description: "Used for AS/400 or iSeries. Other than maintaining legacy systems, I don't see myself developping it further. It's a pitty, because the language is very rich and I became extremely proficient with it" , 
+                    level: "Excellent", 
+                    description: "Used for AS/400 or iSeries. Other than maintaining legacy systems, I don't see myself developing it further. It's a pity, because the language is very rich and I became extremely proficient with it" , 
                     interest: "2/10",
                     order: 9
                 },
                 { 
                     title: "Java & J2EE", 
-                    level: "Excelent", 
+                    level: "Excellent", 
                     description: "Up to Java 8 and 9, mostly using it for API and Microservices, but also to the level of Generics." , 
                     interest: "8/10",
                     order: 8
@@ -39,7 +39,7 @@ const skillsdata =
                 { 
                     title: "Languages", 
                     level: "Very fluent", 
-                    description: "English is the language I think in. Often, over my own mother tongue. French, although I learned it later in life, I also speak it very fluently. Of course Spanish and Catalan, as my mother tongue languages, I master them too.", 
+                    description: "English is the language I think in. Often, over my own mother tongue. French, although I learned it later in life, I also speak it very fluently. Of course, Spanish and Catalan, as my mother tongue languages, I master them too.", 
                     interest: "0    /10",
                     order: 9
                 },
@@ -52,6 +52,7 @@ const skillsdata =
                 },
             ]
         },
+
         {
             language: "sp",
             title: "Ferran Closa Cervera",
