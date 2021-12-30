3e75bb43d0f31ae53d1aa896c3cb8f9d578287ce
ferraclosa <ferraclosa@gmail.com>
2021-12-30 11:36:16 +0100

    spanish

--- src/data/events.js
+++ src/data/events.js
@@ -86,51 +86,51 @@ const itemss = [
     
           {
             title: "1970- 1981",
-            cardTitle: "Music and Violin Studies. Graduation",
+            cardTitle: "Studios de Musica y Violin. Graduación",
             url: "https://ajuntament.barcelona.cat/conservatori/es",
             cardSubtitle:
-              'That followed a dream, after seeing a 9 year old, playing a Sarasate piece on TV. What a journey!',
+              'Eso siguió a un sueño, después de ver a un niño de 9 años, tocando una pieza de Sarasate en la televisión. ¡Qué viaje!',
           },
           {
             title: "June, 1983",
-            cardTitle: "Violinist, Orchestra Sinfonica de Sevilla",
+            cardTitle: "Violinista, Orquesta Sinfónica de Sevilla",
             url: "https://rossevillatv.com",
             cardSubtitle:
-              'I applied and got a place in the Sinfonica de la Maestranza, in Seville. It was my first Musician job.',
+              'Solicité y conseguí una plaza en la Sinfónica de la Maestranza, en Sevilla. Fue mi primer trabajo como músico',
           },
           {
             title: "June, 1984",
-            cardTitle: "Violin Professor, Conservatori Liceu Mataro, Barcelona",
+            cardTitle: "Profesor de Violín, Conservatorio Liceu Mataro, Barcelona",
             url: "https://www.conservatoriliceu.es/escoles/vinculades/escola-de-musica-liceu-mataro/",
             cardSubtitle:
-              'A professor position opened up in Mataro, near my home town, and I applied and got the place and moved back to Barcelona',
+              'Se abrió un puesto de profesor en Mataró, cerca de mi ciudad natal, y solicité y conseguí la plaza y me mudé de nuevo a Barcelona',
           },
           {
             title: "1987-1992",
-            cardTitle: "Violinist, Orchestra Sinfonica del Vallés, Sabadell",
+            cardTitle: "Violinista, Orquesta Sinfónica del Vallés, Sabadell",
             url: "http://www.osvalles.com/es",
             cardSubtitle:
-              'A newly created orchestra provided opportunities. This started a period in my life where I was both a exercing musicien, and a Music Professor.',
+              'Una orquesta de nueva creación brindó oportunidades. Esto comenzó un período en mi vida en el que fui a la vez un musicien y un profesor de música',
           },
           {
             title: "1992",
             cardTitle: "The Mistery, and move to London",
             url: "https://www.enthealth.org/conditions/cholesteatoma/",
             cardSubtitle:
-              'All stories have a mistery. This is mine. I decided to move to London, to get some space between my past and what would my future become.',
+              'Todas las historias tienen un misterio. Esta es la mía. Decidí mudarme a Londres, para conseguir un espacio entre mi pasado y en qué se convertiría mi futuro',
           },
           {
             title: "1992-1994",
             cardTitle: "IT Student, London",
             url: "http://www.osvalles.com/es",
-            cardSubtitle: `I decided to invest into learning a programming language, like COBOL, RPG, Object Pascal and Visual Basic, hoping to find a job. `
+            cardSubtitle: `Decidí invertir en aprender un lenguaje de programación, como COBOL, RPG, Object Pascal y Visual Basic, con la esperanza de encontrar un trabajo.  `
           },
           {
             title: "1994-1997",
             cardTitle: "IT Trainer, Forbes UK, London",
             url: "http://www.forbes.com/es",
-            cardSubtitle: `This was a job for both training and preparing training material for on-site training. I had the initiative to open up the courses to on-line and distance learning candidates. Due to 
-        the huge demand, it proved to be new market for the company `
+            cardSubtitle: `Este fue un trabajo tanto para la capacitación como para la preparación de material de capacitación para la capacitación en el sitio. Tuve la iniciativa de abrir los cursos a candidatos en línea y a distancia. Debido a 
+            la enorme demanda, resultó ser un nuevo mercado para la empresa `
     
           },
     
@@ -138,15 +138,15 @@ const itemss = [
             title: "1998-2007",
             cardTitle: "Software Developper, HSBC UK, London",
             url: "https://www.hsbc.co.uk/",
-            cardSubtitle: `I was head-hunted by one of my previous students at Forbes. Started as a developper, moving up the ladder to more and more senior positions.  `
+            cardSubtitle: `Fui cazado por uno de mis estudiantes anteriores en Forbes. Comenzó como un desarrollador, ascendiendo en la escalera a puestos cada vez más altos.    `
     
           },
           {
             title: "2007- now",
             cardTitle: "Commercial Banking Architecte, HSBC FR, Paris",
             url: "https://www.hsbc.fr/",
-            cardSubtitle: `In France I arrived from HSBC UK, as ETL expert and following a 5 year stay in France as expatriate, they offered me a position as HSBC systems expert and 
-        and Commercial Banking Architect. `
+            cardSubtitle: `'En Francia llegué de HSBC UK, como experto en ETL y después de una estancia de 5 años en Francia como expatriado, me ofrecieron un puesto como experto en sistemas de HSBC y 
+            y Arquitecto de Banca Comercial.      `
     
           },
     ]
