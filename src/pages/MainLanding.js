7ae6a79e0ec20b2509eaef880ff2c1ffebe0faf7
ferraclosa <ferraclosa@gmail.com>
2022-01-21 01:31:34 +0100

    clean

--- src/pages/MainLanding.js
+++ src/pages/MainLanding.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import image from 'assets/icon/favicon.ico'
+// import image from 'assets/icon/favicon.ico'
 
 
 import { mains } from "data/mains";
