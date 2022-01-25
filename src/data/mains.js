ae6ee295c563d7f6ea01fd5578f3c7c9bcfbd880
ferraclosa <ferraclosa@gmail.com>
2022-01-25 17:33:18 +0100

    overflow

--- src/data/mains.js
+++ src/data/mains.js
@@ -7,8 +7,7 @@ const mains = [
         spannedWord: "Closa Cervera", 
         subtitle: "Enterprise Architect, Microservices, Web, Full-Stack development, Trainer",
         description: "Welcome to my website. As you will see my profile is somewhat heterodox, but in my case, this is what brings richness and lateral thinking to the projects I participate in. I'm currently participating on a pan-European project involving Germany, UK, Greece, Spain, Italy, France, Poland and others. However my motto remains 'Embrace Change' as it brings novelty and new possibilities",
-        objectives: `I'm interested in Data Migration, Transfomation projects from Monolith to more distributed systems.  
-        I will also consider projects requiring Java, React and Python developer skills`,
+        objectives: `I'm interested in Data Migration, Transfomation projects from Monolith to more distributed systems. `,
   
         ceo: "CEO of erronea.eu"
     }, 
@@ -19,7 +18,7 @@ const mains = [
         spannedWord: "Closa Cervera", 
         subtitle: "Arquitecto informático de Empresas, Especialista en Microservicios, web, Tecnologías ‘Front and Back end’, Educador",
         description: "Bienvenido a mi página Web. Como veréis mi perfil es algo heterodoxo, pero en mi caso, esto es lo que aporta riqueza y pensamiento lateral. En este momento estoy trabajando en un gran proyecto pan-Europeo. Mi motto es 'Embrace change' como creo que el cambio nos enriquece y nos propone possibilidades",
-        objectives: "Estoy interesado en proyectos de Migración de datos, Transformación de sistemas monolitos hacia Arquitecturas más distribuidas. También considero proyectos como desarrollador Java, React y Python",
+        objectives: "Estoy interesado en proyectos de Migración de datos, Transformación de sistemas monolitos hacia Arquitecturas más flexibles. ",
         ceo: "CEO de erronea.eu"
     }, 
     {
@@ -29,7 +28,7 @@ const mains = [
         spannedWord: "Closa Cervera", 
         subtitle: "Arquitecte Informatic d'Empreses, Especialista en Microserveis, WEB, Tecnologies Front i Back end, Educador",
         description: "Benvingut a la meva pàgina Web. Actualment estic treballant, però estic activament buscant altres opcions i obert a ofertes. Com veureu el meu perfil és heterodox, però en el meu cas, això és el que aporta més riquesa i pensament lateral.",
-        objectives: "Estic interessat amb projectes de Migracio de Dades, Transformacio de sistemes Monolits cap a Arquitectures méschitecturas mas distribuidas. Tambien considero proyectos como dessarrolador Java, React y Python",
+        objectives: "Estic interessat amb projectes de Migracio de Dades, Transformacio de sistemes Monolits cap a Arquitectures méschitecturas mas distribuidas. ",
 
         ceo: "CEO d'erronea.eu"
     }, 
